56fd73c80872db415630d1e7cca9bd21e088f4d6
Simran <mdrsimran@gmail.com>
2021-02-08 12:34:58 -0600

    commit tree aa6ce8271ebd4a6c7476747b527717bb3683db50

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record){
+const value = record.find (r => r.result === "W")
+return !!value ? value.year : undefined
+}
+
+superbowlWin(record)
